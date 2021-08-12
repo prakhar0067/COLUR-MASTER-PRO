52d1c202dee77907f74cd97a28083227d88afa76
prakhar0067 <88768613+prakhar0067@users.noreply.github.com>
2021-08-12 13:04:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+function setup() {
+  createCanvas(400,400);
+
+  box = createSprite(200,200,20,20)
+}
+
+function draw() 
+
+{
+  background(30);
+
+ if(keyIsdown(RIGHT_ARROW))
+ background = red
+  
+
+
+  drawSprites()
+
+}
+
+
+
+
